8c6922f218937a29a4251e44e85a6af732ecdb1b
Motti Gottlieb <mottigottlieb@gmail.com>
2019-11-12 12:46:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 // in index write cards writeCards()
 
         // what gets into here will be iterated
+        
+        writeCards(["Ada", "Brendan", "Ali"], "birthday");
 
 function writeCards() {
   
