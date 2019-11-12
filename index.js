6d508cf3706b3aaecf16908983d7c5332307e7f0
Motti Gottlieb <mottigottlieb@gmail.com>
2019-11-12 12:51:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-// Code your solutions in this file
-
-// in index write cards writeCards()
-
-        // what gets into here will be iterated
-        
-        writeCards(["Ada", "Brendan", "Ali"], "birthday");
-
-function writeCards(name, event) {
-  for (let i = 0; i < writeCards; i++) {
-        
-    return writeCards;
+  
+function writeCards( namesArray, event ) {
+  let thankYouCards = []
+  for ( let i = 0; i < namesArray.length; i++ ) {
+    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
   }
-  
-  
-  
-  
+  return thankYouCards
+}
+
+function countdown( countdown ) {
+  while ( countdown > 0 ) {
+    console.log( countdown );
+    countdown -= 1;
+  }
+  console.log( countdown );
 }
